31b47eec82e910043f540ab0fa15e63ff885202b
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2019-12-15 01:19:10 +0600

    King_Attack

---
+++ 2d shooter v4/Assets/King/King_Attack/King_attack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class King_attack : MonoBehaviour
+{
+    public int health;
+
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
